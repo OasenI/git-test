d382c1f4b5c5ffbe9abf0d24084f7b6ab9e3f516
Oasen <Oasen1023@gmail.com>
2022-08-14 14:26:49 +0800

    third commit

--- src/main/java/com/oasen/git/GitTest.java
+++ src/main/java/com/oasen/git/GitTest.java
@@ -8,5 +8,9 @@ package com.oasen.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello Git!");
+        System.out.println("hello Git2!");
+        System.out.println("hello Git3!");
     }
+
+    
 }
